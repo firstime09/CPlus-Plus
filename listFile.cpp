89c56cb8ecd6203158ea5addb0f939db39c60d40
FFTampinongkol <fftampinongkol09@gmail.com>
2023-02-24 16:23:18 +0700

    Update

--- listFile.cpp
+++ listFile.cpp
@@ -55,3 +55,43 @@ int main(){
 
     return 0;
 }
+
+# Latihan 2 - Next Code
+#include <iostream>
+using namespace std;
+
+struct Node { int data; Node* prev; Node* next; };
+
+Node* head = NULL; Node* tail = NULL;
+
+void addNode(int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    if (head == NULL) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode; } }
+
+void printForward() {
+    Node* current = head;
+    while (current != NULL) {
+        cout << current->data << " "; current = current->next;
+    }
+    cout << endl; }
+
+void printReverse() {
+    Node* current = tail;
+    while (current != NULL) {
+        cout << current->data << " "; current = current->prev;
+    }
+    cout << endl; }
+
+int main(){ addNode(1); addNode(2); addNode(3); addNode(4); addNode(5);
+    cout << "Doubly linked list in forward order: "; printForward();
+    cout << "Doubly linked list in reverse order: "; printReverse();
+    return 0; }
